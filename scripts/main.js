45ecac38b0a4948626af1845a16c76a9b5eafd33
Elizabeth Ladwig <7528001@isd535.org>
2021-09-13 09:11:09 -0500

    no tail to the ball

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,8 @@ let dy = -2;
 
 
 function draw() {
+    // clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     // drawing code 
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI*2);
