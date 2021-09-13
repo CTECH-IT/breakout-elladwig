f12c5466036374752658e2f2ee8334d39da9a624
Elizabeth Ladwig <7528001@isd535.org>
2021-09-13 09:01:07 -0500

    fyhf

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,23 @@
 let canvas = document.getElementById("myCanvas")
 let ctx = canvas.getContext("2d")
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false)
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+let x = canvas.width/2;
+let y = canvasheight-30;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
-//making it move pg11
+let dx = 2;
+let dy = -2;
+
+function draw() {
+    // drawing code 
+    ctx.beginPath();
+    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+    // chnage the x and y values for the ball
+    // x += dx;
+    // y += dy;
+
+}
+setInterval(draw, 10);
