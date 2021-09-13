59f4ebaa27587a2754740c9cbac782286a8603fd
Elizabeth Ladwig <7528001@isd535.org>
2021-09-13 09:16:30 -0500

    cleaned things up

--- scripts/main.js
+++ scripts/main.js
@@ -7,21 +7,27 @@ let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 
+function drawBall() {
+   ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath(); 
+
+}
 
 function draw() {
     // clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // drawing code 
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+    
+    // drawing the ball 
+    drawBall();
 
     // chnage the x and y values for the ball
     x += dx;
     y += dy;
 
 }
+
 
 setInterval(draw, 10);
