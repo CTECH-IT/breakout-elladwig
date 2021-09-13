48a2a40f10c23f97a9591dc3c5d72c7ceadbd569
Elizabeth Ladwig <7528001@isd535.org>
2021-09-13 09:08:04 -0500

    making ball move

--- scripts/main.js
+++ scripts/main.js
@@ -2,22 +2,24 @@ let canvas = document.getElementById("myCanvas")
 let ctx = canvas.getContext("2d")
 
 let x = canvas.width/2;
-let y = canvasheight-30;
+let y = canvas.height-30;
 
 let dx = 2;
 let dy = -2;
 
+
 function draw() {
     // drawing code 
     ctx.beginPath();
-    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.arc(x, y, 10, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 
     // chnage the x and y values for the ball
-    // x += dx;
-    // y += dy;
+    x += dx;
+    y += dy;
 
 }
+
 setInterval(draw, 10);
