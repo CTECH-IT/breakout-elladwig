a8a1d781ad419ecb7c0a02c27b756ffc81d4ea0b
Elizabeth Ladwig <7528001@isd535.org>
2021-09-14 09:27:42 -0500

    added boundaries of the canvas

--- scripts/main.js
+++ scripts/main.js
@@ -57,9 +57,15 @@ function draw() {
     //paddle controls
     if(rightPressed) {
         paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
     }
     else if(leftPressed) {
         paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
     }
 
     drawPaddle();
