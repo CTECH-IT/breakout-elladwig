74720845efd9fbb794f173c218ed715fc581cc6d
Elizabeth Ladwig <7528001@isd535.org>
2021-09-14 09:18:21 -0500

    spaced things out

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,8 @@ function drawBall() {
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath(); 
-function drawPaddle() {
+
+    function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "#0095DD";
